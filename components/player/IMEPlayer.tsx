014eb0526539e4f827fd4f012b1ae30472d86440
NelsonXBT Labs <104361802+NelsonXBT@users.noreply.github.com>
2026-08-01 13:28:15 +0100

    Update IMEPlayer.tsx

--- components/player/IMEPlayer.tsx
+++ components/player/IMEPlayer.tsx
@@ -245,8 +245,8 @@ export default function IMEPlayer({
       <div
             ref={wrapperRef}
             className={`relative overflow-hidden bg-black ${
-                showRotateOverlay
-                ? "fixed left-0 right-0 top-12 bottom-0 z-50 rounded-none overflow-hidden touch-none flex items-center justify-center"
+                isFullscreen
+                ? "fixed inset-0 z-50 flex items-center justify-center rounded-none"
                 : "aspect-video w-full rounded-2xl"
             }`}
             onMouseMove={showControls}
