f12e8b7b5af1c6bec0921653c7f18d409f3da06b
NelsonXBT Labs <104361802+NelsonXBT@users.noreply.github.com>
2026-08-01 12:59:26 +0100

    player

--- components/player/IMEPlayer.tsx
+++ components/player/IMEPlayer.tsx
@@ -209,6 +209,7 @@ export default function IMEPlayer({
       setIsFullscreen(true);
       document.documentElement.classList.add("player-fullscreen");
       
+      
 
       try {
         await (
