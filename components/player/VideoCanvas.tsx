45fe3d16f0dcfd20959c49ee01fe8bbeb4628fa6
NelsonXBT Labs <104361802+NelsonXBT@users.noreply.github.com>
2026-08-03 22:27:05 +0100

    Update VideoCanvas.tsx

--- components/player/VideoCanvas.tsx
+++ components/player/VideoCanvas.tsx
@@ -116,14 +116,10 @@ const VideoCanvas = forwardRef<HTMLVideoElement, Props>(
     }, [src]);
 
     return (
-      <video
-        ref={ref}
-        playsInline
-        preload="metadata"
-        onClick={onClick}
-        className="absolute inset-0 w-full h-full object-contain bg-black"
-        />
-    );
+  <div className="h-64 w-full bg-red-900 text-white flex items-center justify-center">
+    VideoCanvas Loaded
+  </div>
+);
   }
 );
 
