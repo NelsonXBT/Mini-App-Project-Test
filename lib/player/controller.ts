5444bf8a2f9bcf83a7f10bc927e6b309e21e46f6
NelsonXBT Labs <104361802+NelsonXBT@users.noreply.github.com>
2026-08-04 12:59:53 +0100

    Update controller.ts

--- lib/player/controller.ts
+++ lib/player/controller.ts
@@ -3,11 +3,20 @@ let saveCallback: (() => Promise<void>) | null = null;
 export function registerPlayer(
   callback: () => Promise<void>
 ) {
+  console.log("✅ registerPlayer() called");
+
   saveCallback = callback;
 }
 
 export async function savePlayerProgress() {
-  if (saveCallback) {
-    await saveCallback();
+  console.log("➡️ savePlayerProgress() called");
+
+  if (!saveCallback) {
+    console.log("❌ No player registered");
+    return;
   }
+
+  console.log("✅ Calling registered player");
+
+  await saveCallback();
 }
