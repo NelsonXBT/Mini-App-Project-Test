427f163674f27faf117e127b62c226d839811465
NelsonXBT Labs <104361802+NelsonXBT@users.noreply.github.com>
2026-08-04 18:32:52 +0100

    Update page.tsx

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -10,7 +10,7 @@ export default async function CoursesPage() {
     <main className="space-y-4">
       <div className="mx-auto max-w-5xl">
         <h1 className="mb-4 text-2xl font-bold">
-          Courses
+          Coursess
         </h1>
 
         <div className="space-y-4">
