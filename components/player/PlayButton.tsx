4838f63be5cc24be441e5823817481ddb28f03f1
NelsonXBT Labs <104361802+NelsonXBT@users.noreply.github.com>
2026-08-06 01:41:21 +0100

    Update PlayButton.tsx

--- components/player/PlayButton.tsx
+++ components/player/PlayButton.tsx
@@ -16,8 +16,8 @@ export default function PlayButton({
       aria-label={playing ? "Pause video" : "Play video"}
       className="
   flex
-  h-12
-  w-12
+  h-16
+  w-16
   items-center
   justify-center
   rounded-full
