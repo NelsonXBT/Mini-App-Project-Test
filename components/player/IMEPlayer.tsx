a6b834f4a739448aee4c672b0eefaca39028cf8c
NelsonXBT Labs <104361802+NelsonXBT@users.noreply.github.com>
2026-08-06 09:08:37 +0100

    Update IMEPlayer.tsx

--- components/player/IMEPlayer.tsx
+++ components/player/IMEPlayer.tsx
@@ -324,7 +324,7 @@ async function saveCurrentProgress() {
       flex
       items-center
       justify-center
-      bg-white/5
+      bg-white/0.15
       backdrop-blur-sm
       pointer-events-none
       transition-opacity
@@ -334,7 +334,7 @@ async function saveCurrentProgress() {
     <p
       className="
         rounded-full
-        bg-white/20
+        bg-white/0.15
         px-5
         py-2.5
         text-base
